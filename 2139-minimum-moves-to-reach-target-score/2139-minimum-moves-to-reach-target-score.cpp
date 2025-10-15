ec7d7029dd454ece6b865a76829a0cb8c758abf8
siddhardha324765 <24B11CS324@adityauniversity.in>
2025-10-15 21:59:55 +0530

    Time: 0 ms (100%), Space: 7.9 MB (32.93%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
@@ -5,7 +5,7 @@ public:
             return target-1;
         }
         int ans=0;
-        while(target>1){ans++;
+        while(target>1 and maxDoubles!=0){ans++;
             if(target%2==0 and maxDoubles>0){
                 target/=2;
                 maxDoubles--;
@@ -14,6 +14,7 @@ public:
                 target--;
             }
         }
-        return ans;
+        target--;
+        return ans+target;
     }
 };
