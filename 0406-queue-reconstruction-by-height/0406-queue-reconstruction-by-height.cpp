5226d0e6b9750e4f01e5ac18a858adc08ecfe6dd
siddhardha324765 <24B11CS324@adityauniversity.in>
2026-06-25 10:07:07 +0530

    Time: 83 ms (5.3%), Space: 30.1 MB (5.06%) - LeetHub

---
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+ // sorting technique
+bool static com(vector<int> p1, vector<int> p2){
+     return p1[0] > p2[0] || (p1[0] == p2[0] && p1[1] < p2[1]);
+}
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+          sort(people.begin(), people.end(),com);
+        vector<vector<int>> sol;
+        for (auto person : people){
+            sol.insert(sol.begin() + person[1], person);
+        }
+        return sol;
+    }
+};
