015c43ab506f86d2c3d77589b3b28693528e6bc0
siddhardha324765 <24B11CS324@adityauniversity.in>
2026-06-30 19:21:21 +0530

    Time: 399 ms (70.77%), Space: 50 MB (67.94%) - LeetHub

--- 3700-number-of-zigzag-arrays-ii/3700-number-of-zigzag-arrays-ii.cpp
+++ 3700-number-of-zigzag-arrays-ii/3700-number-of-zigzag-arrays-ii.cpp
@@ -1,78 +1,63 @@
 class Solution {
 public:
- static const long long MOD = 1000000007LL;
+    const long long MOD = 1000000007LL;
 
-    vector<vector<long long>> multiply(
-        const vector<vector<long long>>& A,
-        const vector<vector<long long>>& B
-    ) {
+    // Matrix × Matrix
+    vector<vector<long long>> multiplyMatrix( vector<vector<long long>>& A,vector<vector<long long>>& B){
         int sz = A.size();
-
         vector<vector<long long>> C(sz, vector<long long>(sz, 0));
-
-        for (int i = 0; i < sz; i++) {
-            for (int k = 0; k < sz; k++) {
+        for (int i = 0; i < sz; i++){
+            for (int k = 0; k < sz; k++){
                 if (A[i][k] == 0) continue;
-
                 long long cur = A[i][k];
-
-                for (int j = 0; j < sz; j++) {
+                for (int j = 0; j < sz; j++){
                     if (B[k][j] == 0) continue;
-
                     C[i][j] = (C[i][j] + cur * B[k][j]) % MOD;
                 }
             }
         }
-
         return C;
     }
+
+    // Vector × Matrix
+    vector<long long> multiplyVector(vector<long long>& V,vector<vector<long long>>& A){
+        int sz = V.size();
+        vector<long long> C(sz, 0);
+        for (int i = 0; i < sz; i++) {
+            if (V[i] == 0) continue;
+            long long cur = V[i];
+            for (int j = 0; j < sz; j++) {
+                if (A[i][j] == 0) continue;
+                C[j] = (C[j] + cur * A[i][j]) % MOD;
+            }
+        }
+        return C;
+    }
+
     int zigZagArrays(int n, int l, int r) {
         int m = r - l + 1;
         int sz = 2 * m;
-
         vector<vector<long long>> T(sz, vector<long long>(sz, 0));
-
         for (int x = 0; x < m; x++) {
-
-            for (int y = x + 1; y < m; y++) {
+            for (int y = x + 1; y < m; y++)
                 T[x][m + y] = 1;
-            }
 
-            for (int y = 0; y < x; y++) {
+            for (int y = 0; y < x; y++)
                 T[m + x][y] = 1;
-            }
         }
-
-        vector<vector<long long>> result(sz, vector<long long>(sz, 0));
-        for (int i = 0; i < sz; i++) {
-            result[i][i] = 1;
-        }
-
+        vector<long long> result(sz, 1);
         long long power = n - 1;
-
         while (power > 0) {
-            if (power & 1) {
-                result = multiply(result, T);
-            }
+            if (power & 1)
+                result = multiplyVector(result, T);
 
-            T = multiply(T, T);
+            T = multiplyMatrix(T, T);
             power/=2;
         }
-
-        vector<long long> initial(sz, 1);
-
         long long answer = 0;
+        for (long long x : result)
+            answer = (answer + x) % MOD;
 
-        for (int i = 0; i < sz; i++) {
-            long long rowSum = 0;
-
-            for (int j = 0; j < sz; j++) {
-                rowSum = (rowSum + result[i][j]) % MOD;
-            }
-
-            answer = (answer + rowSum) % MOD;
-        }
-
-        return (int)answer;
+        return answer;
     }
 };
